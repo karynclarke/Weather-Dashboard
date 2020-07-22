20e933b3e6d20f6af66700bad4ea12a96a139d02
karynclarke <karynclarke@comcast.net>
2020-07-22 13:00:55 -0700

    rounded numbers no icons

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ $(document).ready(function() {
             .text(text);
         $(".history").prepend(li);
     }
-    //console.log("searching weather");
 
     function searchWeather(searchValue) {
         $.ajax({
@@ -41,7 +40,7 @@ $(document).ready(function() {
             if (history.indexOf(searchValue) === -1) {
                 history.push(searchValue);
                 localStorage.setItem("history", JSON.stringify(history))
-                console.log(history);
+
             }
             var temp = Math.round(data.main.temp);
             console.log(data);
@@ -54,14 +53,18 @@ $(document).ready(function() {
             var tempEl = $("<p>")
                 .addClass("card-text")
                 .text("Temperature: " + temp);
+            let TempEl = Math.round(temp);
             var wind = $("<p>")
                 .addClass("card-text")
-                .text("Wind Speed: " + data.wind.speed + "mph");
+                .text(`Wind Speed: ${parseInt(data.wind.speed)} mph`);
             var humid = $("<p>")
                 .addClass("card-text")
                 .text("Humidity:" + data.main.humidity + "%");
-            var img = $("<img>").attr("src",
-                "https://api.openweathermap.org/img/w" + data.weather[0].icon + ".png"
+            var img = $("<img>").attr(
+                "src",
+                "https://openweathermap.org/img/w" +
+                data.weather[0].icon +
+                ".png"
             );
 
             $("#today").append(card);
@@ -85,76 +88,54 @@ function getForecast(lat, lon) {
     }).then(function(res) {
 
         for (var i = 1; i < 6; i++) {
-            console.log(res.daily[i])
-            var currDay = res.daily[i]
+            console.log(res.daily[i]);
+            var currDay = res.daily[i];
             var col = $("<div>").addClass("col-lg-2");
-            var date = moment.unix(currDay.dt).format('MMMM Do');
+            var date = moment
+                .unix(currDay.dt)
+                .format("MMMM Do");
             var card = $("<div>").addClass("card");
-            var cardBody = $("<div>").addClass("card-body");
-            var title = $("<h2>").addClass("card-title").text(date);
-            var cardBody = $("<div>").addClass("card-body");
+            var cardBody = $("<div>").addClass(
+                "card-body"
+            );
+            var title = $("<h2>")
+                .addClass("card-title")
+                .text(date);
+            var cardBody = $("<div>").addClass(
+                "card-body"
+            );
 
             var tempEl = $("<p>")
                 .addClass("card-text")
-                .text("Temperature: " + currDay.temp.day);
+                .text(
+                    "Temp: " + parseInt(currDay.temp.day) + "F");
+
             var wind = $("<p>")
                 .addClass("card-text")
-                .text("Wind Speed: " + currDay.wind_speed + "mph");
+                .text(
+                    "Wind Speed: " +
+                    parseInt(currDay.wind_speed) +
+                    "mph"
+                );
+
             var humid = $("<p>")
                 .addClass("card-text")
-                .text("Humidity:" + currDay.humidity + "%");
+                .text(
+                    "Humid:" + currDay.humidity + "%"
+                );
 
-
-            $("#forecast").append(col.append(card.append(cardBody.append(title, tempEl, wind, humid))));
-
+            $("#forecast").append(
+                col.append(
+                    card.append(
+                        cardBody.append(
+                            title,
+                            tempEl,
+                            wind,
+                            humid
+                        )
+                    )
+                )
+            );
         }
     })
 }
-
-
-//                 //loop over all forecasts
-//                 for (var i = 0; 1 < data.list.length; i++) {
-//                     //forecast for 3pm
-//                     if (data.list[1].dt.txt.indexOf("15:00:00") !== -1) {
-//                         //elements for card
-//                         var col = $("<div>").addClass("col-md-2");
-//                         var card = $("<div>").addClass("card bg-primary text-white");
-//                         var body = $("<div>").addClass("card-body p-2");
-
-//                         var title = $("<h5>")
-//                             .addClass("card-title")
-//                             .text(newDate(data.list[i].dt.txt).toLocalDateString());
-//                         var img = $("<img>").attr(
-//                             "src",
-//                             "https://openweathermap.org/img/w" +
-//                             data.list[i].weather[0].icon +
-//                             ".png"
-//                         );
-//                         var p1 = $("<p>")
-//                             .addClass("card-text")
-//                             .text("Temp: " + data.list[i].main.temp_max + " f");
-//                         var p2 = $("<p>")
-//                             .addClass("card-text")
-//                             .text("Humidity: " + data.list[i].main.humidity + " % ");
-//                         //merge on page
-//                         col.append(card.append(body.append(title, img, p1, p2)));
-//                         $("forecast.row").append(col);
-//                     }
-//                 }
-//             })
-
-//             .catch(function() {
-//             // catch any errors
-//         }),
-//     });
-
-//     function getUVIndex(lat, lon) {
-//         $.ajax({
-//             type: "GET",
-//             url: "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-//                 searchValue +
-//                 "&appid=95d2250a485b04b30b74e7111f480544",
-//             dataType: "json",
-//             success: function(data) {
-//                 var uv = $("<p>").text("UV Index:  ");
-//                 var btn = $("<span>").addClass("btn bt-sm").text(data.value);
